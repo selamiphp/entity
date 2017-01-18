2544e2ed8880915e4b35551eeb2aa5666c9e1f7b
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-19 02:47:26 +0300

    Date class for DataType added

---
+++ src/DataType/Date.php
@@ -0,0 +1,120 @@
+<?php
+declare(strict_types=1);
+
+namespace Selami\Entity\DataType;
+
+use Selami\Entity\Interfaces\DataTypeInterface;
+use DateTime;
+use Assert\Assertion;
+use BadMethodCallException;
+use InvalidArgumentException;
+
+class Date extends DataTypeAbstract implements DataTypeInterface
+{
+    const DATA_TYPE_ERROR   = 'Error for value "%s" for "%s" : INVALID_TYPE. Must be a date string.';
+    const DATA_FORMAT_ERROR = 'Error for value "%s" for "%s": INVALID_DATE_FORMAT';
+    const DATA_MIN_ERROR    = 'Error for value "%s" for "%s": MIN_VALUE=';
+    const DATA_MAX_ERROR    = 'Error for value "%s" for "%s": MAX_VALUE=';
+
+    protected static $defaults = [
+        'format'    => 'Y-m-d H:i:s',
+        'default'   => 'now',
+        'min'       => null,
+        'max'       => null
+    ];
+
+    protected static $validDateOptions = [
+        'now',
+        'tomorrow',
+        'next week',
+        'next month',
+        'next year',
+        'yesterday',
+        'previous week',
+        'previous month',
+        'previous year',
+    ];
+
+    /**
+     * Date constructor.
+     * @param string $key
+     * @param string $datum
+     * @param array $options
+     */
+    public function __construct(string $key, string $datum, array $options = [])
+    {
+        $this->key = $key;
+        $this->datum = $datum;
+        $this->options = array_merge(self::$defaults, $options);
+        $this->errorMessageTemplate = self::DATA_TYPE_ERROR;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function assert()
+    {
+        $this->isString();
+        $this->checkFormat();
+        $this->checkMin();
+        $this->checkMax();
+        return true;
+    }
+    private function isString()
+    {
+        if (!is_string($this->datum)) {
+            $this->throwException();
+        }
+    }
+
+    private function checkFormat()
+    {
+        $this->errorMessageTemplate = self::DATA_FORMAT_ERROR;
+
+        if (in_array($this->datum, self::$validDateOptions, true)) {
+            return true;
+        }
+        try {
+            Assertion::date($this->datum, $this->options['format']);
+        } catch (BadMethodCallException $e) {
+            $this->throwException();
+        }
+    }
+
+    private function checkMin()
+    {
+        $this->errorMessageTemplate = self::DATA_MIN_ERROR . $this->options['min'];
+        if ($this->options['min'] === null) {
+            return true;
+        }
+        if ($this->datum < $this->options['min']) {
+            $this->throwException();
+        }
+        return true;
+    }
+
+    private function checkMax()
+    {
+        $this->errorMessageTemplate = self::DATA_MIN_ERROR . $this->options['max'];
+        if ($this->options['max'] === null) {
+            return true;
+        }
+        if ($this->datum > $this->options['min']) {
+            $this->throwException();
+        }
+        return true;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize()
+    {
+        try {
+            $this->assert();
+            $date = new DateTime($this->datum);
+        } catch (InvalidArgumentException $e) {
+            $date = new DateTime($this->options['default']);
+        }
+
+        return $date->format($this->options['format']);
+    }
+}
