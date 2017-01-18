346b3ecd088c383bb5fb90f8508d614702e50f29
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-19 02:36:22 +0300

    DataType test abstract added

---
+++ tests/extend/DataType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Selami\Test\Abstracts;
+
+use InvalidArgumentException;
+
+abstract class DataType extends \PHPUnit_Framework_TestCase
+{
+    protected $className;
+    protected $validValue;
+    protected $key;
+    protected $options;
+
+    /**
+     * @test
+     */
+    public function shouldReturnDataTypeInterfaceInstance()
+    {
+        $dataType = new $this->className($this->key, $this->validValue, $this->options);
+        $this->assertInstanceOf($this->className, $dataType);
+    }
+
+    /**
+     * @test
+     * @dataProvider trueProvider
+     *
+     * @param mixed $value
+     * @param array $options
+     */
+    public function shouldReturnTrueForValidDataAssertion($value, $options)
+    {
+        $dataType = new $this->className($this->key, $value, $options);
+        $this->assertTrue($dataType->assert());
+    }
+
+    /**
+     * @test
+     * @dataProvider exceptionProvider
+     *
+     * @param mixed $value
+     * @param array $options
+     * @expectedException InvalidArgumentException
+     */
+    public function shouldThrowExceptionForInvalidDataAssertion($value, $options)
+    {
+        $dataType = new $this->className($this->key, $value, $options);
+        $dataType->assert();
+    }
+
+    /**
+     * @test
+     * @dataProvider defaultsProvider
+     *
+     * @param mixed $value
+     * @param array $options
+     * @param array $expected
+     */
+
+    public function shouldReturnNormalizedValues($value, $options, $expected)
+    {
+        $dataType = new $this->className($this->key, $value, $options);
+        $returnedValue  =$dataType->normalize();
+        $this->assertEquals($expected, $returnedValue);
+    }
+}
