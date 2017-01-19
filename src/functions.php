b178392af3dec90539effae610beb5acdf8bc8f7
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-19 14:49:13 +0300

    getType added to functions

--- src/functions.php
+++ src/functions.php
@@ -2,3 +2,26 @@
 declare(strict_types=1);
 
 namespace Selami\Entity;
+
+/**
+ * A Note:
+ * Since the built-in php function gettype returns "double" variabe type, here is the workaround function
+ * See http://php . net/manual/en/function . gettype . php => Possible values for the returned string are:
+ * "double" (for historical reasons "double" is returned in case of a float, and not simply "float")
+ *
+ * @param mixed     $value
+ * @return string
+ */
+function getType($value)
+{
+    return [
+        'boolean'   => 'boolean',
+        'string'    => 'string',
+        'integer'   => 'integer',
+        'long'      => 'integer',
+        'double'    => 'float',
+        'float'     => 'float',
+        'array'     => 'array',
+        'null'      => 'null'
+    ][strtolower(gettype($value))];
+}
