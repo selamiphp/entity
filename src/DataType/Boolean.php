2135e4edccae184d5925967b13c1f94fde2c30d5
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-19 15:33:32 +0300

    Boolean for DataType added

---
+++ src/DataType/Boolean.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Selami\Entity\DataType;
+
+use Selami\Entity\Interfaces\DataTypeInterface;
+use Assert\Assertion;
+use Assert\AssertionFailedException;
+use InvalidArgumentException;
+
+class Boolean extends DataTypeAbstract implements DataTypeInterface
+{
+    const DATA_TYPE_ERROR   = 'Assertion failed for value "%s" for "%s" : INVALID_TYPE';
+
+    protected static $defaults = [];
+
+    /**
+     * Boolean constructor.
+     * @param string $key
+     * @param mixed $datum
+     * @param array $options
+     */
+    public function __construct(string $key, $datum, array $options = [])
+    {
+        $this->key = $key;
+        $this->datum = $datum;
+        $this->options = array_merge(self::$defaults, $options);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function assert()
+    {
+        try {
+            Assertion::boolean($this->datum);
+        } catch (AssertionFailedException $e) {
+            $this->errorMessageTemplate = self::DATA_TYPE_ERROR;
+            $this->throwException();
+        }
+        return true;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize()
+    {
+        try {
+            $this->assert();
+            return $this->datum;
+        } catch (InvalidArgumentException $e) {
+            return $this->options['default'];
+        }
+    }
+}
