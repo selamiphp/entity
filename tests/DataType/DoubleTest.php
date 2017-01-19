e25c199273f2f00d5e1bec82847e14c0c1cdae8e
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-19 16:00:01 +0300

    Double Class for DataType and its test added

---
+++ tests/DataType/DoubleTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace tests;
+
+use Selami\Test\Abstracts\DataType;
+
+class MyDoubleClass extends DataType
+{
+
+    public function setUp()
+    {
+        $this->className    = '\\Selami\\Entity\\DataType\\Double';
+        $this->validValue   = true;
+        $this->key          = 'ratio';
+        $this->options      = [];
+    }
+
+    public function trueProvider()
+    {
+        return [
+            [0.1],
+            [PHP_INT_MIN/11],
+            [PHP_INT_MAX/11]
+        ];
+    }
+
+    public function exceptionProvider()
+    {
+        return [
+            ['2323'],
+            [1],
+            [PHP_INT_MAX]
+        ];
+    }
+
+    public function defaultsProvider()
+    {
+        return [
+            [0.1, 0.1],
+            [PHP_INT_MAX/11, PHP_INT_MAX/11],
+            [9, 0.5, ['default' => 0.5]]
+        ];
+    }
+}
