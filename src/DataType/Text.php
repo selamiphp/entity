32a4d095c4ca1942125a9cba27d9e3eed0631eed
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-19 17:26:39 +0300

    Text Class for DataType and its tests added

---
+++ src/DataType/Text.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+namespace Selami\Entity\DataType;
+
+use Selami\Entity\Interfaces\DataTypeInterface;
+use Assert\Assertion;
+use Assert\AssertionFailedException;
+
+class Text extends DataTypeAbstract implements DataTypeInterface
+{
+    const DATA_TYPE_ERROR   = 'Assertion failed for value "%s" for "%s" : INVALID_TYPE';
+    const DATA_LENGTH       = 'Assertion failed for value "%s" for "%s" : INVALID_TEXT_LENGTH';
+
+    protected static $defaults = [
+        'default'   => '',
+        'min'       => 0,
+        'max'       => null,
+        'pad'       => 'left',
+        'left_pad'  => ' ',
+        'right_pad' => ' '
+    ];
+
+    protected static $padOptions = [
+        'left'  => STR_PAD_LEFT,
+        'right' => STR_PAD_RIGHT
+    ];
+
+    /**
+     * Boolean constructor.
+     * @param string $key
+     * @param mixed $datum
+     * @param array $options
+     * @throws validArgumentException
+     */
+    public function __construct(string $key, $datum, array $options = [])
+    {
+        $this->key = $key;
+        $this->datum = $datum;
+        $this->checkValidOptions($options);
+        $this->options = array_merge(self::$defaults, $options);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function assert()
+    {
+        $this->isString();
+        $this->checkLength();
+        return true;
+    }
+
+    private function isString()
+    {
+        try {
+            Assertion::string($this->datum);
+        } catch (AssertionFailedException $e) {
+            $this->errorMessageTemplate = self::DATA_TYPE_ERROR;
+            $this->throwException();
+        }
+        return true;
+    }
+
+    private function checkLength()
+    {
+        if ($this->options['max'] !== null) {
+            return $this->checkLengthBetween();
+        }
+        try {
+            Assertion::minLength($this->datum, (int) $this->options['min']);
+        } catch (AssertionFailedException $e) {
+            $this->errorMessageTemplate = self::DATA_LENGTH . 'MIN:' . $this->options['min'];
+            $this->throwException();
+        }
+    }
+    private function checkLengthBetween()
+    {
+        try {
+            Assertion::betweenLength($this->datum, (int) $this->options['min'], (int) $this->options['max']);
+        } catch (AssertionFailedException $e) {
+            $this->errorMessageTemplate = self::DATA_LENGTH
+                . 'MIN:' . $this->options['min']
+                . 'MAX:' . $this->options['max'];
+            $this->throwException();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize()
+    {
+        if (null === $this->datum) {
+            return $this->options['default'];
+        }
+        if ($this->options['max'] !== null) {
+            $length = (int) $this->options['max'];
+            return mb_substr($this->datum, 0, $length);
+        }
+        if (strlen($this->datum) < (int) $this->options['min']) {
+            $padType = self::$padOptions[$this->options['pad']] ?: STR_PAD_RIGHT;
+            $padding = $this->options[$this->options['pad'].'_pad'];
+            return str_pad($this->datum, (int) $this->options['min'], $padding, $padType);
+        }
+        return $this->datum;
+    }
+}
---
+++ tests/DataType/TextTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace tests;
+
+use Selami\Test\Abstracts\DataType;
+
+class MyTextClass extends DataType
+{
+
+    public function setUp()
+    {
+        $this->className    = '\\Selami\\Entity\\DataType\\Text';
+        $this->validValue   = 'PHP is a popular general-purpose scripting language '
+            . 'that is especially suited to web development.';
+        $this->key          = 'description';
+        $this->options      = [];
+    }
+
+    public function trueProvider()
+    {
+        return [
+            ['Selami Entity'],
+            ['Selami Entity', ['max'=> 200]]
+        ];
+    }
+
+    public function exceptionProvider()
+    {
+        return [
+            ['2323', ['min' => 5]],
+            [0.1],
+            ['Selami Entity', ['max'=> 10]]
+        ];
+    }
+
+    public function defaultsProvider()
+    {
+        return [
+            ['Selami Entity', 'Selami Entity'],
+            ['Selami Entity', 'Selami Entity', ['max' => 200]],
+            [
+                null,
+                'PHP is a popular general-purpose scripting language',
+                ['min' => 10, 'default' => 'PHP is a popular general-purpose scripting language']
+            ],
+            [
+                'Selami',
+                'Selami    ',
+                ['min' => 10, 'default' => 'PHP is a popular general-purpose scripting language', 'pad' => 'right']
+            ],
+            ['Selami Entity', 'Selami Ent', ['max' => 10]]
+        ];
+    }
+}
