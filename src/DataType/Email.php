f2c469b50a4cbccfe69a6d7f29c086959095be53
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-20 15:47:58 +0300

    refactored to use DataTypeFilterTrait

--- src/DataType/Email.php
+++ src/DataType/Email.php
@@ -4,53 +4,14 @@ declare(strict_types=1);
 namespace Selami\Entity\DataType;
 
 use Selami\Entity\Interfaces\DataTypeInterface;
-use InvalidArgumentException;
 
 class Email extends DataTypeAbstract implements DataTypeInterface
 {
-    const DATA_FORMAT_ERROR   = 'Assertion failed for value "%s" for "%s" : INVALID_MAIL_ADDRESS_FORMAT';
-
+    use DataTypeFilterTrait;
+    const DATA_TYPE_ERROR   = 'Assertion failed for value "%s" for "%s" : INVALID_MAIL_ADDRESS_FORMAT';
+    protected $filterFlags = FILTER_VALIDATE_EMAIL;
+    protected $sanitizeFlags = FILTER_SANITIZE_EMAIL;
     protected static $defaults = [
         'default'   => ''
     ];
-
-
-    /**
-     * Email constructor.
-     * @param string $key
-     * @param mixed $datum
-     * @param array $options
-     * @throws validArgumentException
-     */
-    public function __construct(string $key, $datum, array $options = [])
-    {
-        $this->key = $key;
-        $this->datum = $datum;
-        $this->checkValidOptions($options);
-        $this->options = array_merge(self::$defaults, $options);
-    }
-    /**
-     * {@inheritdoc}
-     */
-    public function assert()
-    {
-        if (filter_var($this->datum, FILTER_VALIDATE_EMAIL) === false) {
-            $this->errorMessageTemplate = self::DATA_FORMAT_ERROR;
-            $this->throwException();
-        }
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function normalize()
-    {
-        try {
-            $this->assert();
-            return filter_var($this->datum, FILTER_SANITIZE_EMAIL);
-        } catch (InvalidArgumentException $e) {
-            return filter_var($this->options['default'], FILTER_SANITIZE_EMAIL);
-        }
-    }
 }
