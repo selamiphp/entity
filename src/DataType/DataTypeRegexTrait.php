b89bfffa188a8fa2d7af4a8518b60a35308df0c0
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-20 15:47:40 +0300

    refactored DataTypeRegexTrait to use DataTypeFilterTrait

--- src/DataType/DataTypeRegexTrait.php
+++ src/DataType/DataTypeRegexTrait.php
@@ -3,26 +3,10 @@ declare(strict_types=1);
 
 namespace Selami\Entity\DataType;
 
-use InvalidArgumentException;
-use Selami\Entity\Interfaces\DataTypeInterface;
-
 trait DataTypeRegexTrait
 {
-    protected $sanitizeFlags;
-    /**
-     * DataTypeRegexAbstract constructor.
-     * @param string $key
-     * @param mixed $datum
-     * @param array $options
-     * @throws validArgumentException
-     */
-    public function __construct(string $key, $datum, array $options = [])
-    {
-        $this->key = $key;
-        $this->datum = $datum;
-        $this->checkValidOptions($options);
-        $this->options = array_merge(self::$defaults, $options);
-    }
+    use DataTypeFilterTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -37,26 +21,5 @@ trait DataTypeRegexTrait
             $this->throwException();
         }
         return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function normalize()
-    {
-        try {
-            $this->assert();
-            return $this->sanitize();
-        } catch (InvalidArgumentException $e) {
-            return $this->options['default'];
-        }
-    }
-
-    protected function sanitize()
-    {
-        if ($this->sanitizeFlags !== null) {
-            return filter_var($this->datum, $this->sanitizeFlags);
-        }
-        return $this->datum;
     }
 }
