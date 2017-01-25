d0c43aacdb4903de1cfaaa7601183687ea1af589
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-01-26 00:41:36 +0300

    ParserFactory added

---
+++ src/Parser/ParserFactory.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Selami\Entity\Parser;
+
+class ParserFactory
+{
+    /**
+     * @var array
+     */
+    protected static $validParsers = [
+        'ini'   => ConfigIni::class,
+        'json'  => Json::class,
+        'yaml'  => Yaml::class,
+        'xml'   => Xml::class,
+        'php'   => PhpArray::class
+    ];
+    static public function createParser(string $parserType)
+    {
+        if (!in_array($parserType, self::$validParsers, true)) {
+            $message = sprintf(
+                'Invalid parser type: %s. Valid types are case sensitive and possible values are: %s',
+                $parserType,
+                implode(', ', array_keys(self::$validParsers))
+            );
+            throw new UnexpectedValueException($message);
+        }
+        return new self::$validParsers[$parserType]();
+    }
+}
