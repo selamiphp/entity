f1cd797061e2e36d2404f5fb9c07dc8fc83579ee
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-11-29 00:19:32 +0300

    getting properties list added to Model

--- src/Model.php
+++ src/Model.php
@@ -10,13 +10,17 @@ use Selami\Entity\Exception\InvalidArgumentException;
 final class Model
 {
     private $model;
+    private $schema;
     private $requiredFields;
+    private $properties;
 
     public function __construct(string $jsonSchema)
     {
         $this->model = json_decode($jsonSchema);
         if (json_last_error() === JSON_ERROR_NONE) {
             $this->requiredFields = $this->model->required;
+            $this->schema = $this->getSchema($this->model);
+            $this->extractValidProperties();
         }
     }
 
@@ -28,6 +32,22 @@ final class Model
     public function getRequiredFields() : array
     {
         return $this->requiredFields;
+    }
+
+    public function getProperties() : array
+    {
+        return $this->properties;
+    }
+
+    public function extractValidProperties() : void
+    {
+        $rootProperties = get_object_vars($this->schema->resolve()->properties);
+        $this->properties = $this->extractSubProperties($rootProperties);
+    }
+
+    private function extractSubProperties($rootProperties) : array
+    {
+        return array_keys($rootProperties);
     }
 
     public function getSchema(?stdClass $model = null)  : Schema
