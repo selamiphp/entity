ed23be2dec1035e45765f0195c3fe646e05d740f
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-11-29 00:22:29 +0300

    EntityTest fixed for property checking and new exceptions

--- tests/unit/EntityTest.php
+++ tests/unit/EntityTest.php
@@ -27,7 +27,7 @@ class EntityTest extends \Codeception\Test\Unit
         $id = Uuid::uuid4()->toString();
         $entity = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id);
         $entity->name = 'John Doe';
-        $entity->age = 31;
+        $entity->age = 18;
         $entity->email = "john@example.com";
         $entity->website = null;
         $entity->location = new stdClass();
@@ -42,7 +42,7 @@ class EntityTest extends \Codeception\Test\Unit
         $this->assertEquals($id, $entity->entityId());
         $this->assertTrue($entity->validate());
         $arrayFromJson = json_decode(json_encode($entity), true);
-        $this->assertEquals(31, $arrayFromJson['age']);
+        $this->assertEquals(18, $arrayFromJson['age']);
         $this->assertTrue(isset($entity->name));
         unset($entity->name);
         $this->assertFalse(isset($entity->name));
@@ -56,7 +56,7 @@ class EntityTest extends \Codeception\Test\Unit
         $id = Uuid::uuid4()->toString();
         $entity = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id);
         $entity->name = 'John Doe';
-        $entity->age = 31;
+        $entity->age = 18;
         $requiredFields = ['name', 'age'];
         $this->assertTrue($entity->validatePartially($requiredFields));
         $requiredFields = ['name', 'age', 'email'];
@@ -69,25 +69,25 @@ class EntityTest extends \Codeception\Test\Unit
      */
     public function shouldCompareTwoEntityObjectSuccessfully() : void
     {
-        $id = Uuid::uuid4()->toString();
-        $entity1 = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id);
-        $entity2 = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id);
-        $entity3 = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id);
+        $id1 = Uuid::uuid4()->toString();
+        $id2 = $id1;
+        $id3 = Uuid::uuid4()->toString();
+
+        $entity1 = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id1);
+        $entity2 = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id2);
+        $entity3 = Entity::createFromJsonFile(__DIR__.'/../resources/test-schema.json', $id3);
 
         $entity1->name = 'Kedibey';
-        $entity1->details = new stdClass();
-        $entity1->details->age = 11;
-        $entity1->details->type = 'Angora';
+        $entity1->age = 11;
+        $entity1->available_for_hire = true;
 
         $entity2->name = 'Kedibey';
-        $entity2->details = new stdClass();
-        $entity2->details->age = 11;
-        $entity2->details->type = 'Angora';
+        $entity2->age = 11;
+        $entity2->available_for_hire = true;
 
         $entity3->name = 'Kedibey';
-        $entity3->details = new stdClass();
-        $entity3->details->age = 11;
-        $entity3->details->type = 'Van';
+        $entity3->age = 11;
+        $entity3->available_for_hire = true;
 
         $this->assertTrue($entity1->equals($entity2));
         $this->assertFalse($entity1->equals($entity3));
@@ -103,11 +103,44 @@ class EntityTest extends \Codeception\Test\Unit
         $model = Model::createFromJsonFile(__DIR__.'/../resources/test-schema.json');
         $entity = new Entity($model, $id);
         $entity->name = 'John Doe';
-        $entity->age = 31;
+        $entity->age = 18;
         $entity->email = "john@example.com";
         $entity->website = null;
         $entity->validate();
     }
+
+    /**
+     * @test
+     * @expectedException \Selami\Entity\Exception\SettingValueForInvalidPropertyException
+     */
+    public function shouldFailForInvalidInputParameter() : void
+    {
+        $id = Uuid::uuid4()->toString();
+        $model = Model::createFromJsonFile(__DIR__.'/../resources/test-schema.json');
+        $entity = new Entity($model, $id);
+        $entity->name = 'John Doe';
+        $entity->age = 18;
+        $entity->email = "john@example.com";
+        $entity->website = null;
+        $entity->invalidInput = true;
+    }
+
+    /**
+     * @test
+     * @expectedException \Selami\Entity\Exception\OverridingIdentityOfEntityException
+     */
+    public function shouldFailWhenOverridiingId() : void
+    {
+        $id = Uuid::uuid4()->toString();
+        $model = Model::createFromJsonFile(__DIR__.'/../resources/test-schema.json');
+        $entity = new Entity($model, $id);
+        $entity->name = 'John Doe';
+        $entity->age = 18;
+        $entity->email = "john@example.com";
+        $entity->website = null;
+        $entity->id = 1;
+    }
+
 
     /**
      * @test
